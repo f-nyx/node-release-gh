15edb48d5ab554035ca18fb73ae1878eca5ed62c
f-nyx <dnyx@riseup.net>
2020-04-23 17:30:11 -0300

    Bumps submodules versions in the same release command

--- gh-release.js
+++ gh-release.js
@@ -55,6 +55,28 @@ async function resolveNextSemver({ currentRef, owner, repo }) {
   return isHotfix ? 'patch' : 'minor';
 }
 
+/** Iterates the subdirectories searching for submodules and bumps the versions up to the
+ * root module version. It does not create git tags, it just bumps versions and stage all
+ * changes.
+ */
+function bumpModules() {
+  const workingDir = __dirname;
+  const nextVersion = baseVersion();
+  const modules = readdirSync(workingDir, { withFileTypes: true }).filter(inode =>
+    inode.isDirectory() && existsSync(join(workingDir, inode.name, 'package.json'))
+  ).map(inode =>
+    inode.name
+  );
+  console.log(`bumping modules versions up to ${nextVersion}: ${modules.join(', ')}`);
+
+  modules.forEach(moduleName => {
+    shell.cd(join(workingDir, moduleName));
+    exec(`npm version ${nextVersion} --no-git-tag-version`)
+  });
+
+  exec(`git add .`);
+}
+
 /** Bumps the release version based on the last merge commit.
  *
  * If the last merge is from the develop branch, y bumps the minor version. Otherwise, it
@@ -79,7 +101,9 @@ async function release(args) {
   });
 
   console.log(`preparing ${versionBump} release`);
-  const versionResult = exec(`npm version ${versionBump} -m 'New release: %s'`);
+  bumpModules();
+
+  const versionResult = exec(`npm version ${versionBump} --force -m 'New release: %s'`);
 
   if (versionResult.code !== 0) {
     console.info('error preparing the release');
@@ -92,28 +116,6 @@ async function release(args) {
     exec('git push && git push --tags');
     console.log('release successful');
   }
-}
-
-/** Iterates the subdirectories searching for submodules and bumps the versions up to the
- * root module version. It does not create git tags, it just bumps versions and stage all
- * changes.
- */
-function bumpModules() {
-  const workingDir = __dirname;
-  const nextVersion = baseVersion();
-  const modules = readdirSync(workingDir, { withFileTypes: true }).filter(inode =>
-    inode.isDirectory() && existsSync(join(workingDir, inode.name, 'package.json'))
-  ).map(inode =>
-    inode.name
-  );
-  console.log(`bumping modules versions up to ${nextVersion}: ${modules.join(', ')}`);
-
-  modules.forEach(moduleName => {
-    shell.cd(join(workingDir, moduleName));
-    exec(`npm version ${nextVersion} --no-git-tag-version`)
-  });
-
-  exec(`git add .`);
 }
 
 (async function __init() {
@@ -130,11 +132,6 @@ function bumpModules() {
         required: true
       }
     }, release)
-    .command(
-      'bumpModules',
-      'bumps the version in all submodules without tagging or making the release commit in git',
-      bumpModules
-    )
     .demandCommand(1)
     .argv
 }());
